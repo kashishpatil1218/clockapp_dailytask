de290cf94cc6b0a6cfd5ff9633886f163a9ab31f
Kashish patil <156797313+kashishpatil1218@users.noreply.github.com>
2024-08-09 10:07:52 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:clockapp_dailytask/screens/analogePage.dart';
+import 'package:clockapp_dailytask/screens/strapPage.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart';
 
 
+import 'screens/digitalPage.dart';
 import 'screens/buttonPage.dart';
 
 void main() {
@@ -16,7 +19,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        '/':(context)=> const buttonPage(),
+
+        '/':(context)=> const digitalPage(),
+        '/button':(context)=> const buttonPage(),
+        '/clock':(context)=> const analogePage(),
+        '/strap':(context)=> const strapPage(),
+
       },
     );
   }
